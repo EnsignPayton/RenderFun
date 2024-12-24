92b68409ad52ba755dec47049036f597caffdd26
Logan Payton <Logan.Payton@javs.com>
2024-12-24 10:12:00 -0500

    wip

--- native/build.sh
+++ native/build.sh
@@ -6,7 +6,9 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     gcc -shared -o clay.so -fPIC clay.c
 elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin"* || "$OSTYPE" == "win32" ]]; then
     echo "Compiling on Windows..."
-    clang -shared -o clay.dll -DWIN32 clay.c
+    #clang -shared -o clay.dll -DWIN32 clay.c
+    #clang -shared -o clay.dll -DWIN32 clay.c -Xlinker "-def clay.def"
+    clang -shared -fno-inline -o clay.dll clay.c -Wl,/DEF:clay.def
 else
     echo "Unsupported OS: $OSTYPE"
     exit 1
